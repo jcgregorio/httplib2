6934cafc5b1375d6b2561acf14bc36a137d73439
jcgregorio <devnull@localhost>
2006-03-30 13:20:10 +0000

    Applied supplied patch https://sourceforge.net/tracker/?func=detail&atid=818437&aid=1459345&group_id=161082 to update HMAC Digest

--- httplib2/__init__.py
+++ httplib2/__init__.py
@@ -302,10 +302,10 @@ class DigestAuthentication(Authentication):
         qop = self.challenge.get('qop')
         self.challenge['qop'] = ('auth' in [x.strip() for x in qop.split()]) and 'auth' or None
         if self.challenge['qop'] is None:
-            raise UnimplementedDigestAuthOption( _("Unsupported value for qop: %s." % qop))
+            raise UnimplementedDigestAuthOptionError( _("Unsupported value for qop: %s." % qop))
         self.challenge['algorithm'] = self.challenge.get('algorithm', 'MD5')
         if self.challenge['algorithm'] != 'MD5':
-            raise UnimplementedDigestAuthOption( _("Unsupported value for algorithm: %s." % self.challenge['algorithm']))
+            raise UnimplementedDigestAuthOptionError( _("Unsupported value for algorithm: %s." % self.challenge['algorithm']))
         self.A1 = "".join([self.credentials[0], ":", self.challenge['realm'], ":", self.credentials[1]])   
         self.challenge['nc'] = 1
 
@@ -363,12 +363,14 @@ class HmacDigestAuthentication(Authentication):
         if self.challenge['reason'] not in ['unauthorized', 'integrity']:
             self.challenge['reason'] = 'unauthorized'
         self.challenge['salt'] = self.challenge.get('salt', '')
+        if not self.challenge.get('snonce'):
+            raise UnimplementedHmacDigestAuthOptionError( _("The challenge doesn't contain a server nonce, or this one is empty."))
         self.challenge['algorithm'] = self.challenge.get('algorithm', 'HMAC-SHA-1')
         if self.challenge['algorithm'] not in ['HMAC-SHA-1', 'HMAC-MD5']:
-            raise UnimplementedDigestAuthOption( _("Unsupported value for algorithm: %s." % self.challenge['algorithm']))
+            raise UnimplementedHmacDigestAuthOptionError( _("Unsupported value for algorithm: %s." % self.challenge['algorithm']))
         self.challenge['pw-algorithm'] = self.challenge.get('pw-algorithm', 'SHA-1')
         if self.challenge['pw-algorithm'] not in ['SHA-1', 'MD5']:
-            raise UnimplementedDigestAuthOption( _("Unsupported value for pw-algorithm: %s." % self.challenge['pw-algorithm']))
+            raise UnimplementedHmacDigestAuthOptionError( _("Unsupported value for pw-algorithm: %s." % self.challenge['pw-algorithm']))
         if self.challenge['algorithm'] == 'HMAC-MD5':
             self.hashmod = md5
         else:
@@ -378,29 +380,34 @@ class HmacDigestAuthentication(Authentication):
         else:
             self.pwhashmod = sha
         self.key = "".join([self.credentials[0], ":",
-                    self.pwhashmod.new("".join([self.credentials[1], self.challenge['salt']])).hexdigest(),
+                    self.pwhashmod.new("".join([self.credentials[1], self.challenge['salt']])).hexdigest().lower(),
                     ":", self.challenge['realm']
                     ])
         print response['www-authenticate']
         print "".join([self.credentials[1], self.challenge['salt']])
         print "key_str = %s" % self.key
-        self.key = self.pwhashmod.new(self.key).hexdigest()
+        self.key = self.pwhashmod.new(self.key).hexdigest().lower()
 
     def request(self, method, request_uri, headers, content):
         """Modify the request headers"""
-        keys = headers.keys()
+        if 'connection' in headers:
+            hopbyhop = HOP_BY_HOP + [k.strip().lower() for k in headers['connection'].split(',') if k.strip().lower() != 'close']
+        else:
+            hopbyhop = HOP_BY_HOP
+        keys = [k for k in headers.keys() if k.lower() not in hopbyhop]
         keylist = "".join(["%s " % k for k in keys])
         headers_val = "".join([headers[k] for k in keys])
         created = time.strftime('%Y-%m-%dT%H:%M:%SZ',time.gmtime())
-        nonce = sha.new(str(random.getrandbits(512))+created).hexdigest() # This is where the 2.4 requirement comes from
-        request_digest = "%s:%s:%s:%s:%s" % (method, request_uri, nonce, created, headers_val)
+        cnonce = _cnonce()
+        request_digest = "%s:%s:%s:%s:%s" % (method, request_uri, cnonce, self.challenge['snonce'], headers_val)
         print "key = %s" % self.key
         print "msg = %s" % request_digest
-        request_digest  = hmac.new(self.key, request_digest, self.hashmod).hexdigest()
-        headers['Authorization'] = 'HMACDigest username="%s", realm="%s", nonce="%s", uri="%s", created="%s", response="%s", headers="%s"' % (
+        request_digest  = hmac.new(self.key, request_digest, self.hashmod).hexdigest().lower()
+        headers['Authorization'] = 'HMACDigest username="%s", realm="%s", snonce="%s", cnonce="%s", uri="%s", created="%s", response="%s", headers="%s"' % (
                 self.credentials[0], 
                 self.challenge['realm'],
-                nonce,
+                self.challenge['snonce'],
+                cnonce,
                 request_uri, 
                 created,
                 request_digest,
@@ -408,8 +415,8 @@ class HmacDigestAuthentication(Authentication):
                 )
 
     def response(self, response, content):
-        challenge = _parse_www_authenticate(response, 'www-authenticate')['digest']
-        if 'integrity' == challenge.get('reason'):
+        challenge = _parse_www_authenticate(response, 'www-authenticate').get('hmacdigest', {})
+        if challenge.get('reason') in ['integrity', 'stale']:
             return True
         return False
 
