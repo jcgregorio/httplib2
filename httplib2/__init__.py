92088924af0e218bc186c37d1fae2a73af015da4
jcgregorio <devnull@localhost>
2006-07-01 05:53:21 +0000

    Updating docs in preparation for 0.2.0 release

--- httplib2/__init__.py
+++ httplib2/__init__.py
@@ -12,7 +12,9 @@ __author__ = "Joe Gregorio (joe@bitworking.org)"
 __copyright__ = "Copyright 2006, Joe Gregorio"
 __contributors__ = ["Thomas Broyer (t.broyer@ltgt.net)",
     "James Antill",
-    "Xavier Verges Farrero"]
+    "Xavier Verges Farrero",
+    "Jonathan Feinberg",
+    "Blair Zajac"]
 __license__ = "MIT"
 __version__ = "$Rev$"
 
