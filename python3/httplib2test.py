9e603dad1614ec73e02bd0dc9b59d98ec34903b0
jcgregorio@localhost <jcgregorio@localhost>
2010-05-13 23:42:11 -0400

    Fix for issue #94 by Leonard Richardson

--- python3/httplib2test.py
+++ python3/httplib2test.py
@@ -634,6 +634,19 @@ class HttpTest(unittest.TestCase):
         self.assertEqual(response.status, 200)
         self.assertEqual(response.fromcache, False, msg="Should not be from cache")
 
+    def testVaryUnusedHeader(self):
+        # A header's value is not considered to vary if it's not used at all.
+        uri = urllib.parse.urljoin(base, "vary/unused-header.asis")
+        (response, content) = self.http.request(uri, "GET", headers={
+            'Accept': 'text/plain'})
+        self.assertEqual(response.status, 200)
+        self.assertTrue('vary' in response)
+
+        # we are from cache
+        (response, content) = self.http.request(uri, "GET", headers={
+            'Accept': 'text/plain',})
+        self.assertEqual(response.fromcache, True, msg="Should be from cache")
+
     def testHeadGZip(self):
         # Test that we don't try to decompress a HEAD response 
         uri = urllib.parse.urljoin(base, "gzip/final-destination.txt")
