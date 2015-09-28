cf631a73e2f3f43897b65206127ced82382d35f5
Joe Gregorio <jcgregorio@google.com>
2015-09-28 09:49:57 -0400

    Release 0.9.2

--- python2/httplib2/__init__.py
+++ python2/httplib2/__init__.py
@@ -22,7 +22,7 @@ __contributors__ = ["Thomas Broyer (t.broyer@ltgt.net)",
                     "Sam Ruby",
                     "Louis Nyffenegger"]
 __license__ = "MIT"
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 import re
 import sys
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 import sys
 
 pkgdir = {'': 'python%s' % sys.version_info[0]}
-VERSION = '0.9.1'
+VERSION = '0.9.2'
 
 setup(name='httplib2',
         version=VERSION,
